70d524ec763d231afce8979d0bea703785a1eb98
romdy <kimjh0937@gmail.com>
2024-12-19 18:36:01 +0900

    Game창의 스프라이트 뷰 수정, 캐릭터 장애물 충돌 처리 수정

--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -1,6 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 public class Moving : MonoBehaviour
@@ -8,18 +6,17 @@ public class Moving : MonoBehaviour
     private BoxCollider2D boxCollider;
     public LayerMask layerMask;
 
-    public float speed;
-    public int walkCount;
+    public float speed = 7f;      // 기본 이동 속도
+    public int walkCount = 15;    // 한 번 입력으로 이동할 스텝 수
     private int currentWalkCount;
-
 
     private Vector3 vector;
 
-    public float runSpeed;
+    public float runSpeed = 2f;   // 달리기 추가 속도
     private float applyRunSpeed;
-    private bool applyRunFlag = false;
+    //private bool applyRunFlag = false;
 
-    private bool canMove = true;
+    private bool canMove = true;  // 이동 가능 여부
 
     private Animator animator;
 
@@ -33,74 +30,67 @@ public class Moving : MonoBehaviour
     {
         while (Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0)
         {
+            // 달리기 모드 체크
             if (Input.GetKey(KeyCode.LeftShift))
             {
                 applyRunSpeed = runSpeed;
-                applyRunFlag = true;
             }
             else
             {
                 applyRunSpeed = 0;
-                applyRunFlag = false;
             }
 
-
+            // 방향 벡터 설정
             vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
 
+            // 대각선 이동 방지
             if (vector.x != 0)
                 vector.y = 0;
 
+            // 애니메이터 방향 설정
             animator.SetFloat("DirX", vector.x);
             animator.SetFloat("DirY", vector.y);
 
-            RaycastHit2D hit;
-
+            // 충돌 감지
             Vector2 start = transform.position;
-            Vector2 end = start + new Vector2(vector.x * speed * walkCount, vector.y * speed * walkCount);
+            Vector2 end = start + new Vector2(vector.x * speed, vector.y * speed);
 
             boxCollider.enabled = false;
-            hit = Physics2D.Linecast(start, end, layerMask);
+            RaycastHit2D hit = Physics2D.Linecast(start, end, layerMask);
             boxCollider.enabled = true;
 
             if (hit.transform != null)
                 break;
 
+            // 이동 애니메이션 활성화
             animator.SetBool("Walking", true);
 
+            // 이동 처리
             while (currentWalkCount < walkCount)
             {
-                if (vector.x != 0)
-                {
-                    transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
-                }
-                else if (vector.y != 0)
-                {
-                    transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
-                }
-                if (applyRunFlag)
-                    currentWalkCount++;
+                transform.Translate(vector.x * (speed + applyRunSpeed) * Time.deltaTime,
+                                    vector.y * (speed + applyRunSpeed) * Time.deltaTime,
+                                    0);
                 currentWalkCount++;
-                yield return new WaitForSeconds(0.01f);
+                yield return null; // 다음 프레임까지 대기
             }
-            currentWalkCount = 0;
 
-
+            currentWalkCount = 0; // 이동 횟수 초기화
         }
-        animator.SetBool("Walking", false);
-        canMove = true;
+
+        animator.SetBool("Walking", false); // 이동 중지 애니메이션
+        canMove = true; // 이동 가능 상태로 변경
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (canMove)
         {
             if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
             {
-                canMove = false;
+                canMove = false; // 이동 중에는 추가 입력 금지
                 StartCoroutine(MoveCoroutine());
             }
         }
-
     }
 }
